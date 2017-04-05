7203e0a2d66d1ed913d7ae9a78a3587e9f9c5148
Renzo Toma <rtoma@bol.com>
2017-04-05 14:02:00 +0200

    add /curl
    Change-Id: I4f20cba8a5e4b9d0098c822b364a7511c765f64d

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"html"
 	"io"
 	"net/http"
 	"net/http/httputil"
@@ -31,13 +33,42 @@ type MetadataResponse struct {
 }
 
 func inspectHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Request from %s\n\n", r.RemoteAddr)))
+
 	dump, err := httputil.DumpRequest(r, true)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Write(dump)
+}
 
+func curlHandler(w http.ResponseWriter, r *http.Request) {
+	url := r.PostFormValue("url")
+
+	if url == "" {
+		fmt.Fprint(w, `
+<html>
+<form method="POST">
+URL: <input type="text" name="url" size="80">
+<input type="submit" value="Fire!">
+</form>
+</html>`)
+		return
+	}
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(w, "GET failed: %s", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Response to GET %s\n\n", url)
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		fmt.Fprintf(w, "DumpResponse failed: %s", err)
+		return
+	}
+	fmt.Fprint(w, html.EscapeString(string(dump)))
 }
 
 func metadataHandler(w http.ResponseWriter, r *http.Request) {
@@ -112,5 +143,6 @@ func main() {
 	http.HandleFunc("/", helloHandler)
 	http.HandleFunc("/metadata", metadataHandler)
 	http.HandleFunc("/inspect", inspectHandler)
+	http.HandleFunc("/curl", curlHandler)
 	http.ListenAndServe(":8000", nil)
 }
