6a1ad8527c2c677a0c3df8da9d4ca84727197771
Renzo Toma <rtoma@bol.com>
2017-04-05 10:35:32 +0200

    its now a webserver, added gce metadata handler
    Change-Id: I2aa036a533633a74299f480d886f0a54ae9aee40

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
-package main // import "gcp-rover"
+package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"cloud.google.com/go/compute/metadata"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
+
+type MetadataResponse struct {
+	ProjectID        string
+	NumericProjectID string
+}
+
+func metadataHandler(w http.ResponseWriter, r *http.Request) {
+	if !metadata.OnGCE() {
+		io.WriteString(w, "not on gce")
+		return
+	}
+
+	response := &MetadataResponse{}
+	v, _ := metadata.ProjectID()
+	response.ProjectID = v
+	v, _ = metadata.NumericProjectID()
+	response.NumericProjectID = v
+
+	js, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/metadata", metadataHandler)
+	http.ListenAndServe(":8000", nil)
 }
